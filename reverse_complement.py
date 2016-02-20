ac785d45c5158aca8aff88b89dd9102d575699d7
Maxime <Maxime@betsy.local>
2016-02-20 05:00:16 +0100

    reverse_complement
    reverse complement prog

---
+++ reverse_complement.py
@@ -0,0 +1,18 @@
+import sys
+
+def reverse_complement(dna) :
+    dna = dna.upper()
+    '''
+    Reverse complement a DNA string
+    '''
+    dna = dna[::-1]
+    revcom = []
+    complement = {"A" : "T", "T" : "A" , "G" : "C" , "C" : "G"}
+    for letter in dna :
+        for key in complement.keys() :
+            if letter == key :
+                revcom.append(complement[key])
+
+    return "".join(revcom)
+
+print reverse_complement(sys.argv[1])
