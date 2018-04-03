9e7e9e02be8d7247bb1c9dea8981bb03d5d2e532
maxibor <maxime.borry@gmail.com>
2018-04-03 14:47:49 +0200

    adding centrifuge2fasta

--- fasta_length.py
+++ fasta_length.py
@@ -3,20 +3,22 @@
 
 import sys
 
-def fasta_len_count (file) :
-	"""
-	Counts length of sequences in fasta
-	"""
-	mylen = 0
-	fasta_handle = open(file, "r")
-	for line in fasta_handle :
-		line = line.rstrip()
-		try :
-			if line[0] != ">" :
-				mylen += len(line)
-		except IndexError :
-			continue
-    	fasta_handle.close()
-	return int(mylen)
 
-print fasta_len_count(sys.argv[1])
+def fasta_len_count(file):
+    """
+    Counts length of sequences in fasta
+    """
+    mylen = 0
+    fasta_handle = open(file, "r")
+    for line in fasta_handle:
+        line = line.rstrip()
+        try:
+            if line[0] != ">":
+                mylen += len(line)
+        except IndexError:
+            continue
+    fasta_handle.close()
+    return int(mylen)
+
+
+print(fasta_len_count(sys.argv[1]))
